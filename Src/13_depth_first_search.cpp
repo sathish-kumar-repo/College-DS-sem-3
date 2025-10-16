144e9a40da87f5508d3c9e7480f98db3eff360bc
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-10-16 18:49:04 +0530

    Add 13th Code and Src

---
+++ Src/13_depth_first_search.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class Graph
+{
+    int a[10][10];
+    int v[10];
+    int n;
+
+public:
+    void getGraph()
+    {
+        cout << "Enter number of nodes: ";
+        cin >> n;
+
+        cout << "\nEnter adjacency matrix:\n";
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                cin >> a[i][j];
+
+        for (int i = 1; i <= n; i++)
+            v[i] = 0;
+
+        dfs(1); // starting DFS from node 1
+    }
+
+    void dfs(int x)
+    {
+        cout << "Visit: " << x << "\n";
+        v[x] = 1;
+
+        for (int i = 1; i <= n; i++)
+            if (a[x][i] == 1 && !v[i])
+                dfs(i);
+    }
+};
+
+int main()
+{
+    Graph g;
+    g.getGraph();
+
+    return 0;
+}
